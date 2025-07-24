e218957cf78362ed0982af2066bdd62ad7540ffa
motty.n <122011476+mottyN@users.noreply.github.com>
2025-07-24 15:39:29 +0300

    גב

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
+// הפורט צריך להיות מה־ENV כדי לעבוד בענן כמו Render
+const PORT = process.env.PORT || 3000;
 
-// כתובת הקבוצה שלך בווצאפ (החלף בכתובת שלך)
+// כתובת הקבוצה שלך בווצאפ
 const whatsappGroupLink = 'https://chat.whatsapp.com/Ecxmzq9SjWlJmKws4qM9OX?mode=ac_t';
 
+// כל בקשה תופנה לכתובת הוואצאפ
 app.get('*', (req, res) => {
   res.redirect(whatsappGroupLink);
 });
 
 app.listen(PORT, () => {
-  console.log(`השרת פועל על http://localhost:${PORT} ומפנה אוטומטית לקבוצת וואצאפ`);
-});
+  console.log(`השרת פועל על פ
