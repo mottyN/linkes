d99d49996d12557477b2d83859b99d806612a35f
motty.n <122011476+mottyN@users.noreply.github.com>
2025-07-26 21:48:57 +0300

    מח

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const whatsappGroupLink = encodeURI('https://www.kore.co.il/');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(302, {
+  res.writeHead(301, {
     'Location': whatsappGroupLink,
     'Content-Type': 'text/plain'
   });
